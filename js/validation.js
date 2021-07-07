410a03f649d996a039ca085846291a7c5a6167ae
Vlad Pridvorov <vlad.pridvorov@yandex.ru>
2021-07-07 11:56:48 +0300

    added validation for the full name field

--- js/validation.js
+++ js/validation.js
@@ -1,10 +1,43 @@
-const email = document.getElementById('email');
+const form = document.getElementById('main-form');
+const fullName = document.getElementById('full-name');
 
-email.addEventListener('input', function (event) {
-    if(email.validity.patternMismatch){
-        console.log("hello");
+
+
+fullName.addEventListener('input', event => {
+    let valueFullName = fullName.value.trim();
+    if(checkFullName(valueFullName)){
+        setSuccessState(fullName);
+    }//else{
+    //     setErrorState(fullName, 'ФИО не может быть пустым');
+    // }
+});
+
+form.addEventListener('submit', event => {
+    let valueFullName = fullName.value.trim();
+    if(!checkFullName(valueFullName)){
+        event.preventDefault();
+        setErrorState(fullName, 'ФИО не может быть пустым');
     }
-    else{
-        console.log("Success");
+});
+
+function checkFullName(input){
+    if(input === ''){
+        return false;
+    }else{
+        return true;
     }
-}, false)
+}
+
+function setErrorState(input, message){
+    const formControl = input.parentElement;
+    const error = formControl.querySelector('span');
+    error.innerText = message;
+    error.className = 'employee__form-error';
+}
+
+function setSuccessState(input){
+    const parentElement = input.parentElement;
+    const error = parentElement.querySelector('span');
+    error.innerText = '';
+    error.className = '';
+}
